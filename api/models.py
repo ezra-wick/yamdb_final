d739c32eb3a723a64db26ce201d9c1c7a1083449
ezra-wick <ezra.wick@ya.ru>
2021-04-08 13:08:36 +0300

    final

--- api/models.py
+++ api/models.py
@@ -1,80 +1,82 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import Avg
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class UserRole(models.TextChoices):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
 
 
 class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    email = models.EmailField(help_text='email address', unique=True)
-
-    class UserRole:
-        USER = 'user'
-        ADMIN = 'admin'
-        MODERATOR = 'moderator'
-        choices = [
-            (USER, 'user'),
-            (ADMIN, 'admin'),
-            (MODERATOR, 'moderator'),
-        ]
-
-    role = models.CharField(max_length=25, choices=UserRole.choices, default=UserRole.USER)
-    code = models.UUIDField(default=uuid.uuid4, editable=False)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-
-class Genre(models.Model):
-    name = models.CharField(max_length=25)
-    slug = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
+    bio = models.TextField(blank=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=255, choices=UserRole.choices, default=UserRole.USER
+    )
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=25)
-    slug = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name    
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=50, unique=True, blank=False)
-    year = models.IntegerField(null=False)
+    name = models.CharField(max_length=200)
+    year = models.IntegerField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre, blank=True, related_name='titles')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, related_name='titles', null=True
+        Category, null=True, blank=True, 
+        on_delete=models.SET_NULL, related_name='titles'
     )
-    genre = models.ManyToManyField(Genre, related_name='titles')
-    description = models.TextField(max_length=50, null=True)
-    rating = models.IntegerField(default=None, null=True, blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
-        return self.name
-
-    def update_rating(self):
-        self.rating = self.review.all().aggregate(Avg('score'))['score__avg']
-        self.save()
+        return self.description
 
 
 class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
-    score = models.IntegerField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    title_id = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='review')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    score = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)]
+    )
+    pub_date = models.DateTimeField(
+        'review pub date', auto_now_add=True, db_index=True
+    )
 
     def __str__(self):
         return self.text
 
 
 class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    pub_date = models.DateTimeField(auto_now_add=True)
-    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'comment pub date', auto_now_add=True, db_index=True
+    ) 
 
     def __str__(self):
         return self.text
